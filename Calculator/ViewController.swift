3bd6f05278006d9451b824aa03798286e8137f5f
Ryan A. Anderson <ryan.anderson.dhhs@gmail.com>
2022-04-14 22:18:55 -0600

    Adding Functionality to Buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,21 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var stringDisplay : [Int] = []
-    
     private var isFinishedtypingNumber: Bool = true
     
     @IBOutlet weak var displayLabel: UILabel!
-    
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
         
         isFinishedtypingNumber = true
-    
+        
+        guard
+            let number = Double(displayLabel.text!),
+            let calcMethod = sender.currentTitle
+        else { fatalError("Cannotconvert display label text to a double") }
+        
+        switch calcMethod {
+            
+        case ("+/-"):
+            displayLabel.text = String(-number)
+            print(number)
+            
+        case ("AC"):
+            displayLabel.text = "0"
+            print(number)
+            
+        case ("%"):
+            displayLabel.text = String(number/10)
+            print(number)
+            
+        default:
+            print("Error with \(calcMethod) Button")
+        }
     }
-
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
@@ -38,7 +56,5 @@ class ViewController: UIViewController {
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
-    
     }
-
 }
